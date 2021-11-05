47063a800480a49651e1d556e0e16e93d62d6a09
Samuel Amado Ramirez <samuelamadoramirez@gmail.com>
2021-11-04 20:38:59 -0500

    primer commit spring

---
+++ src/main/java/co/edu/unbosque/lagenericiclo4/Lagenericiclo4Application.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.lagenericiclo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lagenericiclo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lagenericiclo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/lagenericiclo4/Lagenericiclo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.lagenericiclo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lagenericiclo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
